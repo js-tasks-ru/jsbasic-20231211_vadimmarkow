0e809715e062683f61d440ef0ccc5384864d5c44
Vadim Markov <vadimmarkow123@mail.ru>
2023-12-17 19:29:14 +0300

    вычислил факториал

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,7 +1,8 @@
 function ucFirst(str) {
-  let slicedStr = str.slice(1,str.length)
-  if(str.indexOf(str.toLowerCase()) != -1) {
+    let slicedStr = str.slice(1,str.length)
+    if(str.indexOf(str.toLowerCase()) != -1) {
       return str.charAt(0).toUpperCase() + slicedStr
-  } else if(!str){
-      return str
-  }
+    } else if(!str){
+        return str
+    }
+}
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,6 +1,7 @@
 function truncate(str, maxlength) {
-  if(str.length > maxlength) {
-      return str.slice(0, maxlength - 1) + "…";
-  } else {
-      return str
-  }
+    if(str.length > maxlength) {
+        return str.slice(0, maxlength - 1) + "…";
+    } else {
+        return str
+    }
+}
